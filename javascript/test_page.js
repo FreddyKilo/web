c3efa818648d6e59d92d1fe82875ea415f1fc609
FreddyKilo <freddy.kilo@gmail.com>
2015-08-20 23:23:04 -0700

    Remove stuff

--- javascript/test_page.js
+++ javascript/test_page.js
@@ -21,22 +21,3 @@ function changeHeader() {
         .setAttribute("data-original-title", "Howdy partner");
     }
 }
-
-window.requestAnimationFrame = window.requestAnimationFrame
-                             || window.mozRequestAnimationFrame
-                             || window.webkitRequestAnimationFrame
-                             || window.msRequestAnimationFrame
-                             || function(f){setTimeout(f, 1000/60)}
-
-var bubble1 = document.getElementById('photo-main')
-// var bubble2 = document.getElementById('bubbles2')
-
-function parallaxMain(){
-var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
-bubble1.style.top = -scrolltop * -.2 + 'px' // move bubble1 at 20% of scroll speed
-// bubble2.style.top = -scrolltop * .5 + 'px' // move bubble2 at 50% of scroll speed
-}
-
-window.addEventListener('scroll', function(){ // on page scroll
-requestAnimationFrame(parallaxMain) // call parallaxMain() on next available screen repaint
-}, false)
