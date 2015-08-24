3a2edd42b6d3d9290a619424b79966b658c9a0cc
fred.kudelka <fred.kudelka@ableengineering.com>
2015-08-24 16:05:20 -0700

    jquery conversion

--- javascript/test_page.js
+++ javascript/test_page.js
@@ -1,23 +1,41 @@
 function changeImage() {
-    var image = document.getElementById('he-man');
-    var title = document.getElementById('title');
-    if (image.src.match("He-Man")) {
-        image.src = "./images/Chuck_Norris.jpg";
-        title.innerHTML = "It's Chuck!";
+    var image = $('#he-man');
+    var title = $('#title');
+    if (image.attr("src").match("He-Man")) {
+        image.attr("src", "./images/Chuck_Norris.jpg");
+        title.text("It's Chuck!");
     } else {
-        image.src = "./images/He-Man.jpg";
-        title.innerHTML = "It's He-Man!";
+        image.attr("src", "./images/He-Man.jpg");
+        title.text("It's He-Man!");
     }
 }
 
 function changeHeader() {
-    if (document.getElementById("wink").innerHTML.match("right eye")) {
-        document.getElementById("wink").innerHTML = "I wink my left eye!";
-        document.getElementById("he-man")
-        .setAttribute("data-original-title", "I have the Power!");
+    if ($("#wink").text().match("right eye")) {
+        $("#wink").text("I wink my left eye!");
+        $("#he-man")
+        .attr("data-original-title", "I have the Power!");
     } else {
-        document.getElementById("wink").innerHTML = "I wink my right eye!";
-        document.getElementById("he-man")
-        .setAttribute("data-original-title", "Howdy partner");
+        $("#wink").text("I wink my right eye!");
+        $("#he-man")
+        .attr("data-original-title", "Howdy partner");
     }
 }
+
+// window.requestAnimationFrame = window.requestAnimationFrame
+//                                  || window.mozRequestAnimationFrame
+//                                  || window.webkitRequestAnimationFrame
+//                                  || window.msRequestAnimationFrame
+//                                  || function(f){setTimeout(f, 1000/60)}
+// var mainPhoto = $('#photo-main');
+// var navBar = $('#freddy');
+
+// function parallaxMain(){
+//     var scrolltop = window.pageYOffset; // get number of pixels document has scrolled vertically
+//     mainPhoto.style.top = scrolltop * .6 + 'px';
+//     navBar.style.top = scrolltop * .6 + 'px';
+//     }
+
+//     window.addEventListener('scroll', function(){ // on page scroll
+//     requestAnimationFrame(parallaxMain) // call parallaxMain() on next available screen repaint
+//     }, false)
