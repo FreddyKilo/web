16dc64d11ff80a97fdfe29c087caa3469fe369e2
FreddyKilo <freddy.kilo@gmail.com>
2015-08-24 23:26:10 -0700

    clean up

--- javascript/test_page.js
+++ javascript/test_page.js
@@ -23,17 +23,18 @@ function changeHeader() {
 }
 
 window.requestAnimationFrame = window.requestAnimationFrame
-                                 || window.mozRequestAnimationFrame
-                                 || window.webkitRequestAnimationFrame
-                                 || window.msRequestAnimationFrame
-                                 || function(f){setTimeout(f, 1000/60)}
-var mainPhoto = $('#photo-main');
+                             || window.mozRequestAnimationFrame
+                             || window.webkitRequestAnimationFrame
+                             || window.msRequestAnimationFrame
+                             || function(f){setTimeout(f, 1000/60)}
+
 function parallaxMain(){
+    var mainPhoto = $('#photo-main');
     var scrollAmount = parseInt(window.pageYOffset);
-    var newTopPhoto = -scrollAmount/3;
-    mainPhoto.css("top", newTopPhoto + "px");
+    var topValue = -scrollAmount/3;
+    mainPhoto.css("top", topValue + "px");
 }
 
-window.addEventListener('scroll', function(){ // on page scroll
-    requestAnimationFrame(parallaxMain) // call parallaxMain() on next available screen repaint
+window.addEventListener('scroll', function(){
+    requestAnimationFrame(parallaxMain)
 }, false)
