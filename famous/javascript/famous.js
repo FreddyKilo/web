2ed0a894246d4170062d2316cb0a5d80c5bb27f0
fred.kudelka <fred.kudelka@ableengineering.com>
2015-09-08 16:57:35 -0700

    add bounce to hover animate

--- famous/javascript/famous.js
+++ famous/javascript/famous.js
@@ -20,68 +20,70 @@ window.addEventListener('scroll', function(){
 
 // Photo animation
 
-var animateMillis = 300;
-var photoHover1 = function() {
-	var isHovered = false;
-	$('#band-promo-1').hover(function() {
-		if(!isHovered) {
-			isHovered = true;
-			$('#band-promo-1').animate({zIndex: "1"}, 0);
-			$('#band-promo-1').animate({width: "200%"}, animateMillis);
-		}
-	}, function() {
-		$('#band-promo-1').animate({width: "100%"}, animateMillis);
-		$('#band-promo-1').animate({zIndex: "0"}, 0);
-		isHovered = false;
+var animateMillis = 200;
+
+var addHover = function($element, onHover, onLeave){
+	var hover = function(){
+		$element.off('mouseenter.blowup');
+		$element.on('mouseleave.blowup',leave);
+		onHover();
+	};
+
+	var leave = function(){
+		$element.off('mouseleave.blowup');
+		onLeave(function(){
+			$element.on('mouseenter.blowup',hover);
+		});
+	};
+
+	$element.on('mouseenter.blowup',hover);
+};
+
+
+var photoHoverZoom1 = function($element){
+	var width = $element.width()+10;
+	console.log(width + "px")
+
+	addHover($element, function(callback) {
+		$element.animate({zIndex: "1"}, 0);
+
+		$element.animate({width: width*2.05 + "px"}, 130, "linear", callback);
+		$element.animate({width: width*1.95 + "px"}, 80);
+		$element.animate({width: width*2 + "px"}, 100);
+
+	}, function(callback) {
+
+		$element.animate({width: width*.95 + "px"}, 120, "linear", callback);
+		$element.animate({width: width + "px"}, 80);
+
+		$element.animate({zIndex: "0"}, 0);
 	});
-}
+};
 
-var photoHover2 = function() {
-	$('#cd-release-1').hover(function() {
-		$('#cd-release-1').animate({zIndex: "1"}, 0);
-		$('#cd-release-1').animate({width: "110%", marginLeft: "0px", marginTop: "150px"}, animateMillis);
-	}, function() {
-		$('#cd-release-1').animate({width: "70%", marginLeft: "150px", marginTop: "200px"}, animateMillis);
-		$('#cd-release-1').animate({zIndex: "0"}, 0);
+var photoHoverZoom2 = function($element){
+	var width = $element.width()+10;
+	console.log(width + "px")
+
+	addHover($element, function(callback) {
+		$element.animate({zIndex: "1"}, 0);
+
+		$element.animate({width: width*1.55 + "px"}, 100, "linear", callback);
+		$element.animate({width: width*1.45 + "px"}, 80);
+		$element.animate({width: width*1.5 + "px"}, 100);
+
+	}, function(callback) {
+
+		$element.animate({width: width*.95 + "px"}, 120, "linear", callback);
+		$element.animate({width: width + "px"}, 80);
+
+		$element.animate({zIndex: "0"}, 0);
 	});
-}
+};
 
-var photoHover3 = function() {
-	$('#band-promo-2').hover(function() {
-		$('#band-promo-2').animate({zIndex: "1"}, 0);
-		$('#band-promo-2').animate({width: "150%", marginLeft: "-100px", marginTop: "120px"}, animateMillis);
-	}, function() {
-		$('#band-promo-2').animate({width: "80%", marginLeft: "90px", marginTop: "230px"}, animateMillis);
-		$('#band-promo-2').animate({zIndex: "0"}, 0);
-	});
-}
 
-var photoHover4 = function() {
-	$('#band-promo-3').hover(function() {
-		$('#band-promo-3').animate({zIndex: "1"}, 0);
-		$('#band-promo-3').animate({width: "150%", marginLeft: "-100px", marginTop: "20px"}, animateMillis);
-	}, function() {
-		$('#band-promo-3').animate({width: "100%", marginLeft: "40px", marginTop: "100px"}, animateMillis);
-		$('#band-promo-3').animate({zIndex: "0"}, 0);
-	});
-}
-
-$(document).ready(photoHover1);
-$(document).ready(photoHover2);
-$(document).ready(photoHover3);
-$(document).ready(photoHover4);
-
-// var clicked = false;
-// var photoHover1 = function() {
-// 	$('#band-promo-1').click(function() {
-// 		if(!clicked) {
-// 			$('#band-promo-1').animate({zIndex: "1"}, 0);
-// 			$('#band-promo-1').animate({width: "200%"}, animateMillis);
-// 			clicked = true;
-// 		} else {
-// 			$('#band-promo-1').animate({width: "100%"}, animateMillis);
-// 			$('#band-promo-1').animate({zIndex: "0"}, 0);
-// 			clicked = false;
-// 		}
-// 	});
-// }
+$(function(){
+	photoHoverZoom1($('#band-promo-1'));
+	photoHoverZoom1($('#cd-release-1'));
+	photoHoverZoom1($('#band-promo-2'));
+	photoHoverZoom2($('#band-promo-3'));
+})
